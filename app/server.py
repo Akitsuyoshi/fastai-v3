8e3d5feba8de79bd246471ccf3f1872c79dcaef1
Tsuyoshi Akiyama <ka090824@gmail.com>
2019-05-09 06:30:35 +0900

    Change var to link and classes

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1MSl-yrCniD-G1VGWFL6CdNrfgHgrNqPy'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['agriculture','artisinal_mine','bare_ground','blooming','clear','cloudy','cultivation','habitation','haze','partly_cloudy','primary','road','selective_logging','water']
 path = Path(__file__).parent
 
 app = Starlette()
